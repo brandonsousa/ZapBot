0d00c47864326cfe1b75d2a5d6f6ba49b07dd4ef
Brandon Sousa da Silva <brdnsousa@gmail.com>
2020-04-22 16:04:43 -0300

    ready template engine and configure, update layout files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 const express = require('express')
-
+const path = require('path')
+const handlebars = require('express-handlebars')
 const app = express()
 
 const routes = require('./routes')
 
 app.use(express.json())
+
+app.engine('handlebars', handlebars({extname: 'handlebars', defaultLayout: 'main' ,layoutsDir: __dirname + '/views/'}))
+app.set('views', path.join(__dirname, 'views/'))
+app.set('view engine', 'handlebars')
+app.use(express.static(path.join(__dirname, 'public/')))
+
+app.get('/', (request, response) =>{
+    return response.render('main')
+})
+
 app.use(routes)
 
 app.listen(3333)
